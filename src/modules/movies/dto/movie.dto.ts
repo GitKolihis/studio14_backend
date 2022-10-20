5662af76e7b17ea7dc93dde2b58d90bbac7e9f7b
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-20 21:38:46 +0100

    added database config for movie table

---
+++ src/modules/movies/dto/movie.dto.ts
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class MovieDTO {
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly image: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly duration: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly episodes: number;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly seriesStart: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly seriesEnd: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly title: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly titleType: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    readonly year: string;
+}
---
+++ src/modules/movies/movie.entity.ts
@@ -0,0 +1,65 @@
+/* eslint-disable prettier/prettier */
+import { Table, Column, Model, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { User } from '../users/user.entity'
+
+@Table
+export class Movie extends Model<Movie> {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    image: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    duration: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    episodes: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    seriesStart: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    seriesEnd: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    title: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    titleType: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    year: string;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false
+    })
+    userId: number;
+
+    @BelongsTo(() => User)
+    user: User;
+
+}
