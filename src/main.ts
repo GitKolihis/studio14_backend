fb6b70752aeca525066c0e4939465cf6fa7c08e3
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-20 18:51:15 +0100

    Finished user authentication

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // Setting global prefix for the app
   app.setGlobalPrefix('api/v1');
+
+  // Endpont Validation
+  app.useGlobalPipes(new ValidateInputPipe());
+
+  // Setting up Swagger Documentation
+  const config = new DocumentBuilder()
+  .setTitle('Studio14 Career Test')
+  .setDescription('RestFul API for Movie App')
+  .setVersion('1.0')
+  .addTag('Endpoints')
+  .addBasicAuth()
+  .build()
+
   
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api/v1', app, document)
+
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly userService: UsersService) {
+        super({
+             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+             ignoreExpiration: false,
+             secretOrKey: process.env.JWTKEY,
+        });
+    }
+
+    async validate(payload: any) {
+        // user's token authentication
+        const user = await this.userService.findOneById(payload.id);
+        if (!user) {
+            throw new UnauthorizedException('Access Denied');
+        }
+        return payload;
+    }
+}
